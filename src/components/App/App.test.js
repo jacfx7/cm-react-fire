65ea3ab202270f08758f600232c3a61a664a49f6
Jon A. Clark <Jon.Clark@bropfs.com>
2019-09-16 11:15:17 -0500

    added tiny key to prod

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
+import configureStore from '../../store/configureStore';
+
+let mockedStore = configureStore([])({});
 
 it('renders without crashing', () => {
   /* const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(<App store/>, div);
   ReactDOM.unmountComponentAtNode(div); */
 });
