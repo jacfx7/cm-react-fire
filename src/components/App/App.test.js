9596810165aaf5435d0d8a6154210a2f28ca35a0
Jon A. Clark <Jon.Clark@bropfs.com>
2019-09-16 12:48:36 -0500

    fixed test to build for now

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './index';
-import configureStore from '../../store/configureStore';
-
-let mockedStore = configureStore([])({});
 
 it('renders without crashing', () => {
   /* const div = document.createElement('div');
