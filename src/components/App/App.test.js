fd5b30216ce29535ee2341469e1237df92343919
Jon A. Clark <Jon.Clark@bropfs.com>
2019-09-16 14:58:51 -0500

    app unit test is working

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+
+import Firebase, { FirebaseContext } from '../Firebase';
 import App from './index';
+import * as ROUTES from '../../constants/routes';
+import HomePage from '../Home';
 
-it('renders without crashing', () => {
-  /* const div = document.createElement('div');
+describe('App Component::', () => {
+  const initialState = {
+    sessionState: {
+      authUser: { roles: [] }
+    }
+  };
+  const mockStore = configureStore(initialState);
+  let store, wrapper;
+  const children = <div>{}</div>;
 
-  ReactDOM.render(<App store/>, div);
-  ReactDOM.unmountComponentAtNode(div); */
+  beforeEach(() => {
+    store = mockStore(initialState);
+    wrapper = mount(
+      <Provider store={store}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <MemoryRouter initialEntries={['/']} initialIndex={0}>
+            <App children={children} />
+          </MemoryRouter>
+        </FirebaseContext.Provider>
+      </Provider>
+    );
+  });
+
+  it('renders without crashing', () => {
+    expect(wrapper.find(App)).toHaveLength(1);
+  });
 });
